212933ca2044de39e4a1e84224cddf33ec60f4b3
David Ospina <J_do_o13@hotmail.com>
2023-02-06 17:53:52 -0500

    add random user
    se añadió la función para seleccionar aleatoriamente un elemento de "arrayDatos"

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+localStorage.setItem('arrayPts', JSON.stringify([]));
 localStorage.setItem('arrayDatos',JSON.stringify( 
                     [{
                         Name:'Lady',
@@ -21,23 +22,42 @@ localStorage.setItem('arrayDatos',JSON.stringify(
                     }]));
 function Rand(){
     return Math.floor(Math.random() * 124)+1900;
-}
-console.log(JSON.parse(localStorage.getItem('arrayDatos')));
+};
+function Init(){
+    let array = JSON.parse(localStorage.getItem('arrayDatos'));
+    let init = Math.floor(Math.random() * array.length);
+    localStorage.setItem('user', JSON.stringify(array[init]));
+};
+Init();
+console.log(JSON.parse(localStorage.getItem('user')));
 var app = new Vue({
     el:'#app',
     data:{
         message:"",
         user:"",
         year:"",
+        score:"",
+        lives:7,
+        lives_u:0,
     },
     methods:{
-        Guardar_User(){
+        Save_User(){
             (this.year<2024)?
             JSON.parse(localStorage.getItem('arrayDatos')).push({
                 Name: this.user,
                 Year: this.year,
             }):
             this.message="";
+        },
+        Save_Player(){
+            JSON.parse(localStorage.getItem('arrayPts')).push({
+                Name: this.user,
+                Score: this.score,
+                Lives: this.lives_u,
+            })
+        },
+        Play(){
+            
         },
     },
 })
